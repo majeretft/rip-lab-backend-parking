914264af99b99329100b53d2220f5e0cc21c90fe
Andrey Bliznev <majeretft@mail.ru>
2022-12-06 03:04:09 +0300

    Add user routes and controller

--- server.js
+++ server.js
@@ -60,6 +60,7 @@ require("./app/routes/movie.routes")(app);
 require("./app/routes/seat.routes")(app);
 require("./app/routes/order.routes")(app);
 require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
