29b6e30a811d2532ec4bd716a7053bef7eae5174
Andrey Bliznev <majeretft@mail.ru>
2022-12-12 17:21:28 +0300

    Remove model and update config

--- app/db.config.js
+++ app/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
   HOST: "localhost",
   USER: "labwork",
   PASSWORD: "1234",
-  DB: "labwork_parking_6",
+  DB: "labwork_parking_7",
   dialect: "mysql",
   pool: {
     max: 5,
--- app/models/users.model.js
+++ app/models/users.model.js
@@ -1,5 +1,14 @@
 module.exports = (sequelize, Sequelize) => {
   const Model = sequelize.define("users", {
+    username: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    password: {
+      type: Sequelize.STRING,
+    },
     name: {
       type: Sequelize.STRING,
     },
